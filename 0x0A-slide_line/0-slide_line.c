7b5a6e696cac0c7242497209881e40ba334d066e
maba-dev <cherif.ba@hotmail.fr>
2023-02-23 22:03:24 +0100

    a function that slides and merges an array of integers

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -1,58 +1,102 @@
 #include "slide_line.h"
+/**
+ * reverse - reverse the array
+ * @line: pointer to the array
+ * @size: size of the array
+ * Return: Nothing.
+ **/
+
+void reverse(int *line, size_t size)
+{
+	size_t i, j;
+
+	i = 0;
+	j = size - 1;
+	while (i < j)
+	{
+		int tmp = line[i];
+
+		line[i] = line[j];
+		line[j] = tmp;
+		i++;
+		j--;
+	}
+}
 
 /**
- * slide_line - slides and merges an array of integers
- * @line: array of integers
+ * slide_zero - moves zeros
+ * @line: pointer to the array
  * @size: size of the array
- * @direction: direction to slide and merge the array
- *
- * Return: 1 on success, 0 on failure
+ * Return: Nothing.
+ **/
+void slide_zero(int *line, size_t size)
+{
+	size_t i;
+	size_t zero = 0;
+
+	i = 0;
+	while (i < size)
+	{
+		if (line[i] == 0)
+			zero++;
+		else if (zero)
+			line[i - zero] = line[i];
+		i++;
+	}
+	while (zero > 0)
+	{
+		line[i - 1] = 0;
+		zero--;
+		i--;
+	}
+}
+
+/**
+ * slide_non_zero - moves non zeros
+ * @line: pointer to the array
+ * @size: size of the array
+ * Return: Nothing.
+ **/
+void slide_non_zero(int *line, size_t size)
+{
+	size_t i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (line[i] == line[i + 1])
+		{
+			line[i] *= 2;
+			line[i + 1] = 0;
+		}
+		i += 2;
+	}
+}
+/**
+ * slide_line - function that slides and merges an array of integers.
+ * @line: points to an array of integers containing
+ * @size: elements, that must be slided & merged to the
+ * @direction: represented by direction
+ * Return: 1 upon success, or 0 upon failure.
  */
-
-
 int slide_line(int *line, size_t size, int direction)
 {
-	int *place_here = NULL;
-	int *left = NULL;
-	int *right = NULL;
 
 	if (direction == SLIDE_LEFT)
 	{
-		place_here = line;
-		left = place_here;
-
-		while (left < line + (size - 1))
-		{
-			while (*left == 0 && left < line + (size - 1))
-			{
-				left++;
-			}
-			right = left + 1;
-			while (right < line + (size))
-			{
-				if (*right == *left)
-				{
-					*place_here = *left * 2;
-					if (place_here != left)
-					{
-						*left = 0;
-					}
-					*right = 0;
-					place_here++;
-					break;
-				}
-				else
-				{
-					right++;
-				}
-			}
-			left++;
-		}
-		if (*(line + size - 1) && !*place_here)
-		{
-			*place_here = *(line + size - 1);
-			*(line + size - 1) = 0;
-		}
+		slide_zero(line, size);
+		slide_non_zero(line, size);
+		slide_zero(line, size);
+		return (1);
 	}
-	return (1);
+	else if (direction == SLIDE_RIGHT)
+	{
+		reverse(line, size);
+		slide_zero(line, size);
+		slide_non_zero(line, size);
+		slide_zero(line, size);
+		reverse(line, size);
+		return (1);
+	}
+	return (0);
 }
